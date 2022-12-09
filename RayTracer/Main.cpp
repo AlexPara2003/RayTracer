8870c2236152a12baa0052178edfa22bc524703b
AlexPara2003 <apara@student.neumont.edu>
2022-12-09 08:22:23 -0700

    Added balls

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -1,21 +1,31 @@
 #include "Renderer/Renderer.h"
 #include "Object/Sphere.h"
 #include "Object/Scene.h"
+#include "Object/Plane.h"
 #include <iostream>
+
+void InitScene01(Scene& scene);
+void InitScene02(Scene& scene);
 
 int main(int, char**)
 {
+
+	const int width = 800;
+	const int height = 600;
+	const int samples = 1;
+	const float fov = 30;
+
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(600, 300);
+	renderer.CreateWindow(width, height);
 
-	Canvas canvas(600, 300, renderer);
-	Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, 600 / (float)300);
-	Scene scene;
+	Canvas canvas(width, height, renderer);
+	//Camera camera({ 0, 4, 4 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, width / (float)height);
+	Camera camera({ 13, 2, 3 }, { 0, 0, 0 }, { 0, 1, 0 }, fov, width / (float)height);
+	Scene scene{{1, 0.5f, 0.5f}, {0.5f, 0.5f, 1}};
+	InitScene02(scene);
 
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 1, 0 })));
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0.5f, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0.2f, 0, 0.2f })));
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 1, 1, 0.2f })));
+	//scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 1, 1, 0.2f })));
 
 	bool quit = false;
 	while (!quit)
@@ -39,7 +49,7 @@ int main(int, char**)
 
 		//render scene
 		canvas.Clear({ 0, 0, 0, 1 });
-		renderer.Render(canvas, scene, camera);
+		renderer.Render(canvas, scene, camera, samples);
 
 		canvas.Update();
 
@@ -48,4 +58,60 @@ int main(int, char**)
 	}
 	renderer.Shutdown();
 	return 0;
+}
+//P
+void InitScene01(Scene& scene)
+{
+	//Objects
+	scene.AddObject(std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
+
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0.5f, -1 }, 0.5f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.1f)));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 1, 0.5f, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0.2f, 0, 0.2f })));
+
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 3, 0.5f, -1 }, 0.5f, std::make_unique<Dielectric>(color3{ 1.0f, 1.0f, 1.0f }, 2.0f)));
+
+	//Light
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 5, 5.0f, 3 }, 1.0f, std::make_unique<Emissive>(color3{ 40, 40, 40 })));
+}
+
+void InitScene02(Scene& scene)
+{
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 1, 0 }, 1.0f, std::make_unique<Dielectric>(color3{ 1.0f, 1.0f, 1.0f }, 1.5f)));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ -4, 1, 0 }, 1.0f, std::make_unique<Lambertian>(color3{ 0.4f, 0.2f, 0.1f })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 4, 1, 0 }, 1.0f, std::make_unique<Metal>(color3{ 0.7f, 0.6f, 0.5f }, 0.0f)));
+
+	scene.AddObject(std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, std::make_unique<Lambertian>(color3{ 0.5f, 0.5f, 0.5f })));
+
+	for (int x = -11; x < 11; x++)
+	{
+		for (int z = -11; z < 11; z++)
+		{
+			glm::vec3 center(x + 0.9f * random01(), 0.2f, z + 0.9f * random01());
+
+			if ((center - glm::vec3{ 4, 0.2, 0 }).length() > 0.9f)
+			{
+				std::unique_ptr<Material> material;
+
+				float choose_mat = random01();
+				if (choose_mat < 0.8f) // diffuse
+				{
+					color3 albedo = color3{ random01(), random01(), random01() } *color3{ random01(), random01(), random01() };
+					material = std::make_unique<Lambertian>(albedo);
+				}
+				else if (choose_mat < 0.95) // metal
+				{
+					color3 albedo = color3{ random(0.5f, 1), random(0.5f, 1), random(0.5f, 1) };
+					float fuzz = random(0.0f, 0.5f);
+					material = std::make_unique<Metal>(albedo, fuzz);
+				}
+				else // glass
+				{
+					color3 albedo = color3{ 1, 1, 1 };
+					material = std::make_unique<Dielectric>(albedo, 1.5f);
+				}
+
+				scene.AddObject(std::make_unique<Sphere>(center, 0.2f, std::move(material)));
+			}
+		}
+	}
 }
--- RayTracer/Renderer/Renderer.cpp
+++ RayTracer/Renderer/Renderer.cpp
@@ -1,85 +1,94 @@
 #include "Renderer.h"
-#include <iostream>
+#include "../Math/Ray.h"
+#include "../Math/MathUtils.h"
 #include "../Object/Object.h"
 #include "../Object/Scene.h"
+#include <iostream>
 
 bool Renderer::Initialize()
 {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	{
-		std::cout << "SDL Error: " << SDL_GetError() << std::endl;
-		return false;
-	}
-	return true;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDLError:" << SDL_GetError() << std::endl;
+        return false;
+    }
+    return true;
 }
+
 void Renderer::Shutdown()
 {
-	if (m_window) SDL_DestroyWindow(m_window);
-	if (m_renderer) SDL_DestroyRenderer(m_renderer);
-	SDL_Quit();
+    if (m_window) SDL_DestroyWindow(m_window);
+    if (m_renderer) SDL_DestroyRenderer(m_renderer);
+    SDL_Quit();
 }
+
 bool Renderer::CreateWindow(int width, int height)
 {
-	m_window = SDL_CreateWindow("Ray Tracer", 100, 100, width, height, SDL_WINDOW_SHOWN);
-	if (m_window == nullptr)
-	{
-		std::cout << "SDL Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return false;
-	}
-	m_renderer = SDL_CreateRenderer(m_window, -
-		1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (m_renderer == nullptr)
-	{
-		std::cout << "SDL Error: " << SDL_GetError() << std::endl;
-		return false;
-	}
-	return true;
+    m_window = SDL_CreateWindow("Ray Tracer", 100, 100, width, height, SDL_WINDOW_SHOWN);
+
+    if (m_window == nullptr)
+    {
+        std::cout << "SDLError:" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return false;
+    }
+
+    m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (m_renderer == nullptr)
+    {
+        std::cout << "SDLError:" << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
 void Renderer::CopyCanvas(const Canvas& canvas)
 {
-	SDL_RenderCopy(m_renderer, canvas.m_texture, nullptr, nullptr);
+    SDL_RenderCopy(m_renderer, canvas.m_texture, nullptr, nullptr);
 }
 
 void Renderer::Present()
 {
-	SDL_RenderPresent(m_renderer);
+    SDL_RenderPresent(m_renderer);
 }
 
-void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera)
+void Renderer::Render(Canvas& canvas, Scene& scene, Camera& camera, int samples)
 {
-	// camera / viewport 
-	glm::vec3 lowerLeft{ -2, -1, -1 };
-	glm::vec3 eye{ 0, 0, 0 };
-	glm::vec3 right{ 4, 0, 0 };
-	glm::vec3 up{ 0, 2, 0 };
+    for (int y = 0; y < canvas.m_height; y++)
+    {
+        for (int x = 0; x < canvas.m_width; x++)
+        {
+            color3 color{ 0, 0, 0 };
+            for (int s = 0; s < samples; s++)
+            {
 
-	for (int y = 0; y < canvas.GetHeight(); y++)
-	{
-		for (int x = 0; x < canvas.GetWidth(); x++)
-		{
-			glm::vec2 point = glm::vec2{ x, y } / glm::vec2{ canvas.m_width, canvas.m_height };
-			// flip y 
-			point.y = 1.0f - point.y;
+                // get normalized (0 - 1) u, v coordinates from screen x and y 
+                // add random value (0-1) to screen x and y for anti-aliasing  
+                glm::vec2 point = glm::vec2{ random01() + x, random01() + y } / glm::vec2{ canvas.m_width, canvas.m_height };
 
-			// create ray from camera 
-			Ray ray = camera.PointToRay(point);
+                // flip y 
+                point.y = 1.0f - point.y;
 
-			// cast ray into scene, get color 
-			RaycastHit raycastHit;
-			color3 color = scene.Trace(ray, 0.001f, 1000.0f, raycastHit, 5);
-			canvas.DrawPoint({ x, y }, color4(color, 1));
+                // create ray from camera 
+                Ray ray = camera.PointToRay(point);
 
-
-		}
-	}
+                // cast ray into scene 
+                RaycastHit raycastHit;
+                // add trace color value to color 
+                color += scene.Trace(ray, 0.001f, 1000.0f, raycastHit, 5);
+            }
+            color = color / (color3)samples;
+            canvas.DrawPoint({ x, y }, color4(color, 1));
+        }
+    }
 }
 
 color3 Renderer::GetBackgroundFromRay(const Ray& ray)
 {
-	glm::vec3 direction = glm::normalize(ray.direction);
-	float t = 0.5f * (direction.y + 1.0f);
+    glm::vec3 direction = glm::normalize(ray.direction);
+    float t = 0.5f * (direction.y + 1.0f);
 
-	return lerp(color3{ 1.0f }, color3{ 0.5f, 0.7f, 1.0f }, t);
+    return lerp(color3{ 1.0f }, color3{ 0.5f, 0.7f, 1.0f }, t);
 }
